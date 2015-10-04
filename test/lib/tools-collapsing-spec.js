14688458fdf7a1dcd25a832aba5df548e2f19984
Alex Banks <alex@moonhighway.com>
2015-10-04 12:08:53 -0700

    Update tools-collapsing-spec.js
    Removes tests for unused functions addItems and zeroDiff.

--- test/lib/tools-collapsing-spec.js
+++ test/lib/tools-collapsing-spec.js
@@ -2,18 +2,9 @@ import tools from '../../src/tools';
 import chai from 'chai';
 import sinon from 'sinon';
 var { expect } = chai;
-var { addItems, zeroDiff, zeroRemove, zeroFill, addNeighbors, collapseRow, collapseRight, collapseLeft } = tools;
+var { zeroRemove, zeroFill, addNeighbors, collapseRow, collapseRight, collapseLeft } = tools;
 
 describe('Tools : collapsing', () => {
-
-    describe('addItems(current, next)', () => {
-        it('doubles like numbers', () => expect(addItems(2, 2)).to.equal(4));
-        it('does not double unlike numbers', () => expect(addItems(2, 4)).to.equal(2));
-    });
-
-    describe('zeroDiff(oldArray, newArray', () => {
-        it('Adds a 0 after numbers that are different', () => expect(zeroDiff([2, 2, 4, 4], [4, 2, 8, 4])).to.deep.equal([4, 0, 8, 0]));
-    });
 
     describe('addNeighbors(row)', () => {
         it('adds like numbers', () => expect(addNeighbors([2, 2, 4, 4], zeroRemove, addItems, zeroDiff)).to.deep.equal([4, 8]));
